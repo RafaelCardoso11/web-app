54fda5ec4f557886a6e35bde240d3bd99bd94b48
ANDRE RAFAEL CARDOSO REIS <dev.rafaelcardoso@gmail.com>
2024-07-28 20:34:52 -0300

    feat: use templates interpolation ejs

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -2,15 +2,23 @@
 
 import { Command } from "commander";
 import { createProject } from "../lib/create-project";
+import { displayBanner } from "../utils/displayBanner";
 
 const program = new Command();
 
 program
   .version("1.0.0")
-  .description("CLI para gerar projetos NestJS + Next.js")
+  .description("CLI para gerar projetos NestJS + Next.js");
+
+program.hook("preAction", () => {
+  displayBanner("PROJECT - PSDIT");
+});
+
+program
+  .command("create")
+  .description("Cria um novo projeto")
   .argument("<project-name>", "Nome do projeto")
-  .action((projectName) => {
-    createProject(projectName);
-  });
+  .option("-d, --description <description>", "Descrição do projeto")
+  .action((projectName) => createProject(projectName));
 
 program.parse(process.argv);
--- src/lib/create-project.ts
+++ src/lib/create-project.ts
@@ -1,14 +1,30 @@
 import fs from "fs-extra";
 import path from "path";
+import { renderEJSFile } from "../utils/renderEJSFile";
 
-export function createProject(projectName: string) {
+const ejsFiles = ["package.json", "README.md"];
+
+export const createProject = async (projectName: string) => {
+
+  
   const projectPath = path.join(process.cwd(), projectName);
   const templatePath = path.join(__dirname, "../templates", "app");
+
   try {
-    fs.mkdirSync(projectPath);
-    fs.copySync(templatePath, projectPath);
-    console.log(`Projeto ${projectName} criado com sucesso!`);
-  } catch (err) {
-    console.error(`Erro ao criar o projeto: ${err}`);
+    await fs.ensureDir(projectPath);
+    await fs.copy(templatePath, projectPath);
+
+    const filesWithEjs = ejsFiles.map(async (file) => {
+      const fullPath = path.join(templatePath, file);
+      const newFilePath = path.join(projectPath, file);
+      await renderEJSFile(fullPath, { PROJECT_NAME: projectName }, newFilePath);
+    });
+
+    await Promise.all(filesWithEjs);
+
+    console.log("✨ Projeto criado com sucesso! ✨");
+
+  } catch (error) {
+    console.error(`Erro ao criar o projeto: ${error}`);
   }
-}
+};
