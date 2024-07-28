254517005d37f762f7a3ffed8e862d912cef2832
ANDRE RAFAEL CARDOSO REIS <dev.rafaelcardoso@gmail.com>
2024-07-28 16:26:09 -0300

    feat: criacao do projeto nextjs + nestjs

---
+++ templates/app/src/app/main.ts
@@ -0,0 +1,26 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import next from 'next';
+import { parse } from 'url';
+import { Request, Response } from 'express';
+
+async function bootstrap() {
+  const dev = process.env.NODE_ENV !== 'production';
+  const PORT = 3000;
+
+  const nextApp = next({ dev });
+  const handle = nextApp.getRequestHandler();
+
+  await nextApp.prepare().then(async () => {
+    const app = await NestFactory.create(AppModule);
+
+    app.use((req: Request, res: Response) => {
+      const parsedUrl = parse(req.url, true);
+
+      handle(req, res, parsedUrl);
+    });
+
+    await app.listen(PORT);
+  });
+}
+bootstrap();
